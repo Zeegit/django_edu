8cf81c68140b282b3281089eaa7e50189460633e
Zeegit <sergey.zemskov@gmail.com>
2023-10-03 14:26:07 +0300

    templates

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,3 +1,22 @@
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+
 # Create your views here.
+from django.template.defaulttags import lorem
+
+
+def shop_index(request: HttpRequest) -> HttpResponse:
+    products = [
+        ("Пыво", 2),
+        ("Воттка", 4),
+        ("Рооом", 6),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
+    # return HttpResponse("<h1>Hello</h1>")
