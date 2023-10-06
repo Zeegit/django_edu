78005bd4c00d4053aa03f896c390ba092705b8dc
Zeegit <sergey.zemskov@gmail.com>
2023-10-06 15:37:40 +0300

    mixins

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
+
+from .admin_mixins import ExportAsCSVMixin
+from .models import Product, Order
+
 
 # Register your models here.
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+
+@admin.action(description="Archive")
+def mark_archived(modeladmin: admin.ModelAdmin, request:HttpRequest, queryset: QuerySet):
+    queryset.update(archived=True)
+
+@admin.action(description="Unrchive")
+def mark_unarchived(modeladmin: admin.ModelAdmin, request:HttpRequest, queryset: QuerySet):
+    queryset.update(archived=False)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin, ExportAsCSVMixin):
+    actions = [mark_unarchived, mark_archived, "export_csv"]
+    inlines = [OrderInline, ]
+    list_display = ["pk", "name", "description_short", "description_short_admin", "price", "discount", "created_at",
+                    "archived"]
+    list_display_links = ["pk", "name"]
+    ordering = ["-pk", "name", ]
+    search_fields = "name", "description",
+    fieldsets = [
+        ("Text", {"fields": ("name", "description", ), }),
+        ("Price", {"fields": ("price", "discount", ), "classes": ("collapse","wide",), }),
+        ("extra", {"fields": ("archived", ), "classes": ("collapse", "wide",),
+                   "description":"Extra option" }),
+    ]
+
+    def description_short_admin(self, obj: Product) -> str:
+        return obj.description if len(obj.description) < 20 else obj.description[:20] + "..."
+
+
+class ProdictInlineT(admin.TabularInline):
+    model = Order.products.through
+
+
+class ProdictInlineS(admin.StackedInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [ProdictInlineT, ProdictInlineS, ]
+    list_display = ["pk", "delivery_address", "promocode", "created_at", "user_verbose"]
+    list_display_links = ["pk", "delivery_address"]
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
+
+    # ordering = ["-pk", "name",]
+    # search_fields = "name", "description",
+
+    # def description_short_admin(self, obj: Product) -> str:
+    #    return obj.description if len(obj.description) < 20 else obj.description[:20] + "..."
+
+# admin.site.register(Product, ProductAdmin)
--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -11,12 +11,16 @@ class Product(models.Model):
 
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
-    price = models.DecimalField(default=0,  max_digits=8, decimal_places=2)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     discount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
-    def __str__(self):
-        return f"Product {self.name} {self.description}"
+
+    def description_short(self) -> str:
+        return self.description if len(self.description) < 48 else self.description[:48] + "..."
+
+    def __str__(self) -> str:
+        return f"Product(pk={self.pk}, name={self.name!r})"
 
 
 class Order(models.Model):
@@ -25,3 +29,6 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     products = models.ManyToManyField(Product, related_name="orders")
+
+    def __str__(self) -> str:
+        return f"Order(pk={self.pk}, delivery_address={self.delivery_address!r}, promocode={self.promocode})"
